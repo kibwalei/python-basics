3841b00e0bffe414974ba95c7547aca319c45327
kibwalei <kibwaleianthony@gmail.com>
2023-03-22 19:20:54 +0300

    [prime numbers 22nd march]

---
+++ prime_numbers.py
@@ -0,0 +1,8 @@
+
+# num=int(input("Enter number of prime numbers: "))
+num1= int(input("Enter the last number :"))
+for i in range (num1):
+    if i%2!=0 and i%5!=0 and i%7!=0 and i%3!=0 :
+        # print(i)
+        # if i==2 and i==3 and i==5 and i==7:
+            print(i)
